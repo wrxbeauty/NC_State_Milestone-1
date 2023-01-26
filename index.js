62761d8a52f7fe11a76a4e99254363aca2b4589c
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-01-25 19:32:30 -0500

    Second commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function masterMain() {
+    let pegs = document.getElementById('pegs')
+
+
+}
+
+function newSequence() {
+
+}
+
+newGrid({
+    rows: 10,
+    colors: 6,
+        colors : ['purple', 'pink', 'coral', 'orange', 'dark yellow', 'yellow']
+})
---
+++ notes.js
@@ -0,0 +1,4 @@
+// pegs, peg-console, peg boolean
+// buttons: start, pegs "onclick", submit row
+// show ratio of correct, misplaced, and false
+
