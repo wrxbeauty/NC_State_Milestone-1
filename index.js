76412efec9d17db17c65a0187260094e76975eea
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-01-30 20:59:21 -0500

    added js functions for master

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
+const btnSelector = document.querySelectorAll('.pegs')
 
+let final = randomMaster();
+const colors = ['pink', 'coral', 'orange', 'yellow'];
 
-function masterMain() {
-    let pegs = document.getElementById('pegs')
-
-
+function randomMaster() {
+    const final = [...new Array(3)].map(color => {
+        const random = Math.floor(math.random() * Math.floor(colors.length))
+        return colors[random];
+    })
+    console.log(final)
+    return final;
 }
 
-function newSequence() {
-
-}
-
-newGrid({
-    rows: 3,
-    colors: 6,
-        colors : ['purple', 'pink', 'coral', 'orange', 'dark yellow', 'yellow']
+btnSelector.forEach(btn => {
+    const color = btn.classList[1];
+    btn.addEventListener('click', () => selectColor(color))
 })
+
+function selectColor(color) {
+    console.log(color)
+}
+
+
+
+// function masterMain() {
+//     let pegs = document.getElementById('pegs')
+//     pegs.forEach(peg => {
+//         peg.value
+        
+//     });
+
+// }
+
+// function newSequence() {
+
+// }
+
+// newGrid({
+//     rows: 3,
+//     colors: 4,
+//         colors : ['pink', 'coral', 'orange', 'yellow']
+// })
