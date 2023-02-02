115f1915f41fa03d7340d5df72a72d1d712ee910
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-02-01 21:27:44 -0500

    added JS function for filling in rows

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
-const btnSelector = document.querySelectorAll('.pegs')
+const pegs = document.getElementById('pegs')
+let rowNumber = 1
+let selectedColor
+console.log(pegs)
+let choices = []
 
+for (const child of Array.from(pegs.children)) {
+    console.log(child)
+    child.addEventListener('click', (e) => {
+        selectedColor = e.target.classList[0]
+        choices.push(selectedColor)
+        if (choices.length === 3) {
+            guessRow()
+        }
+    })
+}
+
+const colors = ['#d65db1', '#ff6f91', '#ff9671', '#f9f871'];
 let final = randomMaster();
-const colors = ['pink', 'coral', 'orange', 'yellow'];
 
 function randomMaster() {
     const final = [...new Array(3)].map(color => {
-        const random = Math.floor(math.random() * Math.floor(colors.length))
+        const random = Math.floor(Math.random() * Math.floor(colors.length))
         return colors[random];
     })
     console.log(final)
     return final;
 }
 
-btnSelector.forEach(btn => {
-    const color = btn.classList[1];
-    btn.addEventListener('click', () => selectColor(color))
-})
+function guessRow() {
+    console.log(choices)
+    const row1 = document.getElementById(`row-${rowNumber}`)
+    Array.from(row1.children).forEach((child, i) => {
+        console.log(child)
 
-function selectColor(color) {
-    console.log(color)
+        child.getElementsByClassName.backgroundColor = choices[i]
+    })
+    choices = []
+    rowNumber++
 }
-
-
-
-// function masterMain() {
-//     let pegs = document.getElementById('pegs')
-//     pegs.forEach(peg => {
-//         peg.value
-        
-//     });
-
-// }
-
-// function newSequence() {
-
-// }
-
-// newGrid({
-//     rows: 3,
-//     colors: 4,
-//         colors : ['pink', 'coral', 'orange', 'yellow']
-// })
